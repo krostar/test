299ba9971e63c8b05279300c026bb3520dd2aad1
Alexis Destrez <krostar@users.noreply.github.com>
2025-05-10 12:23:30 +0200

    Use synergy's justfile configuration

--- go.mod
+++ go.mod
@@ -2,14 +2,12 @@ module github.com/krostar/test
 
 go 1.24.0
 
-toolchain go1.24.2
-
 require (
 	github.com/google/go-cmp v0.7.0
-	golang.org/x/tools v0.32.0
+	golang.org/x/tools v0.33.0
 )
 
 require (
 	golang.org/x/mod v0.24.0 // indirect
-	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
 )
--- nix/repo/data.nix
+++ nix/repo/data.nix
@@ -12,49 +12,56 @@ in {
     goimports.excludes = builtins.map (f: "${f}/*") invalidGoCode;
     gofumpt.excludes = builtins.map (f: "${f}/*") invalidGoCode;
   };
-  ci.linters.golangci-lint.linters = {
-    exclusions = {
-      rules = [
-        {
-          # to test the package we hardcode things like !true to get representation of such expression
-          path = "internal/message/from_bool_test\.go";
-          linters = ["gocritic" "govet" "revive" "staticcheck" "stylecheck" "depguard"];
-          text = "boolExprSimplify|dupSubExpr|nilness|bool-literal-in-expr|constant-logical-expr|dupArg|ST1019|SA4000|QF1001|SA4013|import 'reflect' is not allowed";
-        }
-        {
-          # to test the package we hardcode things like !true to get representation of such expression
-          path = "internal/compare/testify/compare_test\.go";
-          linters = ["gocritic" "revive" "staticcheck" "testifylint"];
-          text = "dupSubExpr|bool-literal-in-expr|constant-logical-expr|SA4000|S1040|useless-assert|require-error";
-        }
-        {
-          path = "internal/code/get_caller_call_expr_test\.go|internal/code/parse_package_ast_test\.go";
-          linters = ["staticcheck"];
-          text = "SA5011";
-        }
-        {
-          path = "internal/testing\.go";
-          linters = ["iface"];
-          text = "interface TestingT is declared but not used within the package";
-        }
-        {
-          path = "double/spy_record\.go";
-          linters = ["depguard"];
-          text = "import 'reflect' is not allowed";
-        }
-      ];
-      paths = invalidGoCode;
-    };
-    settings = {
-      depguard = {rules.test = lib.mkForce null;}; # test rules are meant to protect against using other dependencies
-      revive.rules =
-        builtins.map (
-          v:
-            if v.name == "function-result-limit"
-            then v // {arguments = [4];}
-            else v
-        )
-        deps.synergy.result.data.harmony.ci.linters.golangci-lint.linters.settings.revive.rules;
+
+  ci = {
+    testers.go.enable = true;
+    linters.golangci-lint = {
+      enable = true;
+      linters = {
+        exclusions = {
+          rules = [
+            {
+              # to test the package we hardcode things like !true to get representation of such expression
+              path = "internal/message/from_bool_test\.go";
+              linters = ["gocritic" "govet" "revive" "staticcheck" "stylecheck" "depguard"];
+              text = "boolExprSimplify|dupSubExpr|nilness|bool-literal-in-expr|constant-logical-expr|dupArg|ST1019|SA4000|QF1001|SA4013|import 'reflect' is not allowed";
+            }
+            {
+              # to test the package we hardcode things like !true to get representation of such expression
+              path = "internal/compare/testify/compare_test\.go";
+              linters = ["gocritic" "revive" "staticcheck" "testifylint"];
+              text = "dupSubExpr|bool-literal-in-expr|constant-logical-expr|SA4000|S1040|useless-assert|require-error";
+            }
+            {
+              path = "internal/code/get_caller_call_expr_test\.go|internal/code/parse_package_ast_test\.go";
+              linters = ["staticcheck"];
+              text = "SA5011";
+            }
+            {
+              path = "internal/testing\.go";
+              linters = ["iface"];
+              text = "interface TestingT is declared but not used within the package";
+            }
+            {
+              path = "double/spy_record\.go";
+              linters = ["depguard"];
+              text = "import 'reflect' is not allowed";
+            }
+          ];
+          paths = invalidGoCode;
+        };
+        settings = {
+          depguard = {rules.test = lib.mkForce null;}; # test rules are meant to protect against using other dependencies
+          revive.rules =
+            builtins.map (
+              v:
+                if v.name == "function-result-limit"
+                then v // {arguments = [4];}
+                else v
+            )
+            deps.synergy.result.data.harmony.ci.linters.golangci-lint.linters.settings.revive.rules;
+        };
+      };
     };
   };
 }
