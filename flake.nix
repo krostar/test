0d4a78e0aa6607d7c54eb272de61f0d45c486e25
Alexis Destrez <krostar@users.noreply.github.com>
2026-07-02 08:53:03 +0200

    chore: update dependencies

--- flake.nix
+++ flake.nix
@@ -1,16 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    synergy = {
-      url = "github:krostar/synergy";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.follows = "synergy/nixpkgs";
+    synergy.url = "github:krostar/synergy";
   };
 
   outputs = {synergy, ...} @ inputs:
     synergy.lib.mkFlake {
       inherit inputs;
       src = ./.nix;
-      eval.synergy.restrictDependenciesUnits.synergy = ["harmony"];
+      eval.synergy.restrictDependenciesUnits.synergy = ["harmony" "krostar"];
     };
 }
--- go.mod
+++ go.mod
@@ -1,13 +1,13 @@
 module github.com/krostar/test
 
-go 1.24.0
+go 1.25.0
 
 require (
 	github.com/google/go-cmp v0.7.0
-	golang.org/x/tools v0.42.0
+	golang.org/x/tools v0.47.0
 )
 
 require (
-	golang.org/x/mod v0.33.0 // indirect
-	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/mod v0.37.0 // indirect
+	golang.org/x/sync v0.21.0 // indirect
 )
